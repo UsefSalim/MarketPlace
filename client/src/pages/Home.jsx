aa96ffffb84b75fcd651f06e893d170339bc4c69
xelorNpm <salimguithub@gmail.com>
2021-05-30 16:03:41 +0100

    setup client add material ui and basic router

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,6 @@
-
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TopBar from "../components/navigation/TopBar";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -23,17 +17,9 @@ export default function Home() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Xelor
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <>
+      <TopBar />
+      <nav>Home</nav>
+    </>
   );
 }
